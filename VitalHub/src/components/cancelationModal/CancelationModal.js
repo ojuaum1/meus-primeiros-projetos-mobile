9e9912d662d1071abd77ee70ec171ac5e0546cae
João Vitor Oliveira Santos <48342168883@info.local>
2024-03-19 10:38:09 -0300

    :tada: por hj foi so

--- VitalHub/src/components/cancelationModal/CancelationModal.js
+++ VitalHub/src/components/cancelationModal/CancelationModal.js
@@ -4,9 +4,48 @@ import { Title } from "../title/Style"
 import { Button, ButtonSecondary, ButtonSecondaryTitle } from "../button/Style"
 import { ButtonTittle } from "../buttonTitle/Style"
 
-export const CancellationModal = ({ visible, setShowModalCancel, ...rest}) => {
-    return(
+// import notification
+import * as Notifications from "expo-notifications"
 
+Notifications.requestPermissionsAsync();
+
+// definir como as notificacoes devem ser tratadas quando recebidas 
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        // mostra o alerta quando a notificacao for recebida
+        shouldShowAlert: true,
+        // reproduz som
+        shouldPlaySound: true,
+        shouldSetBadge: false,
+    }),
+});
+
+export const CancellationModal = ({ visible, setShowModalCancel, ...rest }) => {
+
+    const CancelationNotification = async () => {
+
+        // obtem o status das permissoes
+        const { status } = await Notifications.getPermissionsAsync();
+        // verifica se o usuario concedeu permissao para a notificacao
+        if (status !== "granted") {
+            alert("Você não concedeu permissão para as notificações");
+            return;
+        }
+
+        // agendar uma notificacao para ser exibida apos 5 segundos 
+        await Notifications.scheduleNotificationAsync({
+            content: {
+                title: "Olá mundo",
+                body: "Criando uma POC para implementar notificação",
+                sound: "notification_message.wav", // utilizar o som carregado
+            },
+            trigger: {
+                seconds: 5
+            }
+        });
+    }
+    return (
+        
         <Modal {...rest} visible={visible} transparent={true} animationType="fade">
 
             <PatientModal>
@@ -24,11 +63,11 @@ export const CancellationModal = ({ visible, setShowModalCancel, ...rest}) => {
                         <ButtonSecondaryTitle>
                             Cancelar
                         </ButtonSecondaryTitle>
-                    </ButtonSecondary> 
+                    </ButtonSecondary>
 
                 </ModalContent>
             </PatientModal>
-  
+
         </Modal>
     )
 }
